1ead2d999e3bffc3f01cb82eb71e465038f86821
pauloborini <owerride@hotmail.com>
2022-10-27 14:40:02 -0300

    teste no initial_screen

--- lib/screens/initial_screen.dart
+++ lib/screens/initial_screen.dart
@@ -11,22 +11,30 @@ class InitialScreen extends StatefulWidget {
   State<InitialScreen> createState() => _InitialScreenState();
 }
 
-
 class _InitialScreenState extends State<InitialScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          toolbarHeight: 290,
-          elevation: 0,
-          backgroundColor: const Color(0xFF19191A),
-          titleSpacing: 0,
-          title: Column(children: const [
-            MobflixTitle(),
-            BannerHighlightFront(),
-            RowCategoryFront(),
-          ])),
-      body: const PostListFront(),
+      body: NestedScrollView(
+        floatHeaderSlivers: false,
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+                forceElevated: innerBoxIsScrolled,
+                expandedHeight: 0,
+                toolbarHeight: 290,
+                elevation: 0,
+                backgroundColor: const Color(0xFF19191A),
+                titleSpacing: 0,
+                title: Column(children: const [
+                  MobflixTitle(),
+                  BannerHighlightFront(),
+                  RowCategoryFront(),
+                ])),
+          ];
+        },
+        body: const PostListFront(),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.of(context).pushReplacementNamed("/post_screen");
@@ -40,3 +48,31 @@ class _InitialScreenState extends State<InitialScreen> {
     );
   }
 }
+
+// @override
+// Widget build(BuildContext context) {
+//   return Scaffold(
+//     appBar: AppBar(
+//         toolbarHeight: 290,
+//         elevation: 0,
+//         backgroundColor: const Color(0xFF19191A),
+//         titleSpacing: 0,
+//         title: Column(children: const [
+//           MobflixTitle(),
+//           BannerHighlightFront(),
+//           RowCategoryFront(),
+//         ])),
+//     body: const PostListFront(),
+//     floatingActionButton: FloatingActionButton(
+//       onPressed: () {
+//         Navigator.of(context).pushReplacementNamed("/post_screen");
+//       },
+//       backgroundColor: const Color(0xff7a60fe),
+//       child: const Icon(
+//         Icons.add,
+//         size: 40,
+//       ),
+//     ),
+//   );
+// }
+// }
